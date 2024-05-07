bfc7f3484a6c0046cda7a94d50ab0625ca8874b4
skipvicente <skipvicente2@aol.com>
2024-05-07 17:34:04 -0500

    Import from laptop note errors still need correction. 5/7/2024 5:30

--- src/Module_02_Vince.java
+++ src/Module_02_Vince.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
-public class Main {
+public class Module_02_Vince {
     public static void main(String[] args) {
 
     JFrame myFrame = new JFrame("My Test Sample Frame UI");
@@ -18,16 +17,22 @@ public class Main {
     // Define Menu Items
     JMenuItem menuItem1 = new JMenuItem("Print Date/Time");
     JMenuItem menuItem2 = new JMenuItem("Write to File");
-    JMenuItem menuItem3 = new JMenuItem("Change Background");
-    JMenuItem menuItem4 = new JMenuItem("Exit");
+    JMenuItem menuItem3 = new JMenuItem("Change Background  ");
+
+    JMenuItem menuItem4 = new JMenuItem("   Exit");
+    //menuItem4.setMargin(new Insets(10,10,10, 10));
 
     // Add Menu Items to Menu
         menuBar.add(menuItem1);
         menuBar.add(menuItem2);
         menuBar.add(menuItem3);
+        //menuBar.add(new JMenu("|"));
+        //menuBar.add(new JSeparator());
+        //menuBar.add( Box.createHorizontalStrut( 30 ) );
+        //menuBar.addSeparator();
         menuBar.add(menuItem4);
 
-    JTextArea textArea = new JTextArea("Sample Text", 10, 1);
+    JTextArea textArea = new JTextArea("", 10, 1);
         myFrame.add(textArea, BorderLayout.CENTER);
 
     // Menu Items Actions
@@ -36,15 +41,20 @@ public class Main {
         try (FileWriter writer = new FileWriter("log.txt")) {
             writer.write(textArea.getText());
             JOptionPane.showMessageDialog(myFrame, "Text written to file");
-        } catch (IOException ioException) {
+        }
+
+        catch (IOException ioException) {
             ioException.printStackTrace();
         }
     });
+
+        // Color changes randomly
         menuItem3.addActionListener(e -> {
         Color randomColor = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
         textArea.setBackground(randomColor);
     });
 
+        // Exit button action
         menuItem4.addActionListener(e -> System.exit(0));
 
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
