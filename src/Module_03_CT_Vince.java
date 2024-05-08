0b14785e46b3e1336c30830f0dcbe7b332dcd344
skipvicente <skipvicente2@aol.com>
2024-05-07 20:42:04 -0500

    Update Setup 5/7/2024 @ 8:41 PM milestone

--- src/Module_03_CT_Vince.java
+++ src/Module_03_CT_Vince.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.Period; //obtains a quantity or amount of time in terms of years, months and days.
-import java.time.temporal.ChronoUnit;
+import java.time.temporal.ChronoUnit; //use to calculate the days
 
 public class Module_03_CT_Vince {
     public static void main(String[] args) {
         // Create my JFrame
         JFrame jFrame = new JFrame("Age Calculator");
-        jFrame.setSize(700, 200);
+        jFrame.setSize(600, 200);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -25,25 +25,23 @@ public class Module_03_CT_Vince {
 
         // Add Action when button is clicked
         calcButton.addActionListener(e -> {
-            //int age;
 
             String birthDateString = birthDateField.getText();
             LocalDate birthDate = LocalDate.parse(birthDateString);
             LocalDate now = LocalDate.now(); /* getting current date to compare */
             System.out.println(now); /* current date displays YYYY-MM-DD */
             int ageYears = Period.between(birthDate, now).getYears(); /* we now compare the dates birth year to current year */
-            /* int ageDays = Period.between(birthDate, now).getDays();  we now compare the dates WRONG*/
             long ageDays = ChronoUnit.DAYS.between(birthDate , now); /* we now calculate the number of days */
             System.out.println(ageDays);
-            ageLabelYears.setText("Your age is approximately " + ageYears + " years old.");
+            ageLabelYears.setText("Your age is approximately " + ageYears + " years old and");
             ageLabelYears.setVerticalAlignment(JLabel.BOTTOM);
-            ageLabelDays.setText("Your age is approximately " + ageDays + " days old.");
+            ageLabelDays.setText(" approximately " + ageDays + " days old.");
             ageLabelDays.setVerticalAlignment(JLabel.BOTTOM);
-        });
-
+       }
+        );
 
         // Add Controls to Panel
-        myPanel.add(new JLabel("Enter birth date YYYY-MM-DD): "));
+        myPanel.add(new JLabel("Please enter birth date YYYY-MM-DD "));
         myPanel.add(birthDateField);
         myPanel.add(calcButton);
         myPanel.add(ageLabelYears);
@@ -52,7 +50,6 @@ public class Module_03_CT_Vince {
         // Add Panel to JFrame
         jFrame.add(myPanel);
         jFrame.setVisible(true);
-
 
     }
 }
