84005ebd49c48c4308604751ffa4cb80f919b7d8
skipvicente <skipvicente2@aol.com>
2024-05-07 20:09:45 -0500

    Update Setup 5/7/2024 @ 8:08 PM milestone with number of days pending DOB to current

--- src/Module_03_CT_Vince.java
+++ src/Module_03_CT_Vince.java
@@ -12,31 +12,39 @@ public class Module_03_CT_Vince {
         jFrame.setSize(800, 600);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         //Create my Panel
         JPanel myPanel = new JPanel();
 
         //Create some panel controls
-        jTextField birthDateField = new jTextField(10);
-        JButton calcButton = new JButton("Calculate Age: )");
-        JLabel ageLabel = new JLabel();
+        JTextField birthDateField = new JTextField(10);
+        JButton calcButton = new JButton("Calculate Age");
+        JLabel ageLabelYears = new JLabel();
+        JLabel ageLabelDays = new JLabel();
 
         // Add Action when button is clicked
         calcButton.addActionListener(e -> {
+            //int age;
+
             String birthDateString = birthDateField.getText();
             LocalDate birthDate = LocalDate.parse(birthDateString);
-            LocalDate now = LocalDate.now();
-            int age = Period.between(birthDate, now).getYears();
-            ageLabel.setText("Your age is " + age);
+            LocalDate now = LocalDate.now(); /* getting current date to compare */
+            System.out.println(now); /* current date displays YYYY-MM-DD */
+            int ageYears = Period.between(birthDate, now).getYears(); /* we now compare the dates birth year to current year */
+            int ageDays = Period.between(birthDate, now).getDays(); /* we now compare the dates */
+            ageLabelYears.setText("Your age is approximately " + ageYears + " years old.");
+            ageLabelDays.setText("Your age is approximately " + ageDays + " days old.");
         });
 
         // Add Controls to Panel
-        mypanel.add(new JLabel("Enter birth date YYYY-MM-DD): "));
-        mypanel.add(birthDateField);
-        mypanel.add(calcButton);
-        mypanel.add(ageLabel);
+        myPanel.add(new JLabel("Enter birth date YYYY-MM-DD): "));
+        myPanel.add(birthDateField);
+        myPanel.add(calcButton);
+        myPanel.add(ageLabelYears);
+        myPanel.add(ageLabelDays);
 
         // Add Panel to JFrame
-        jFrame.add(panel);
+        jFrame.add(myPanel);
         jFrame.setVisible(true);
     }
 }
