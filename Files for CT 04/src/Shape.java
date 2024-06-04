00b390d88029dd4468b17d697fa2698df107cd96
skipvicente <skipvicente2@aol.com>
2024-06-03 20:42:58 -0500

    Updates for CT 04 8:42

---
+++ Files for CT 04/src/Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape {
+    public abstract double surface_area();
+    public abstract double volume();
+}
+
+class Sphere extends Shape {
+
+    private double radius;
+    public Sphere (double radius){
+        this.radius - radius;
+    }
+
+}
+
