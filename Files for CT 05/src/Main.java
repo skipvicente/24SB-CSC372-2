fbfc7d12a503f2065d25d09aaaee116c24d4e5ba
skipvicente <skipvicente2@aol.com>
2024-06-03 22:38:56 -0500

    Updates for CT 05 10:38

---
+++ Files for CT 05/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr01 = new Scanner(System.in);
+
+        int[] userNumbers = new int[5]; /* Only using 5 because we only want to permit 5 elements in array */
+        int count = 1;
+
+        System.out.println("Enter number " + count + " of 5 " + "\n");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            userNumbers[i] = scnr01.nextInt();
+        }
+
+        int product = calculateUserNumbers(userNumbers, 4);
+
+        System.out.println("The product of your numbers is: " + product);
+
+        scnr01.close();
+
+        }
+    }
+}
---
+++ Files for CT 05/src/NumberProductProgram.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NumberProductProgram {
+
+
+    public static int calculateUserNumbers(int[] numbers, int index) {
+
+        if (index < 0) {
+            return 1;
+        }
+        // Recursive calculate product of numbers
+        return numbers[index] * calculateUserNumbers(numbers, index - 1);
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scnr01 = new Scanner(System.in);
+
+            int[] userNumbers = new int[5]; /* Only using 5 because we only want to permit 5 elements in array */
+            int count = 1;
+
+            System.out.println("Enter number " + count + " of 5 " + "\n");
+
+            for (int i = 0; i < 5; i++) {
+                System.out.print("Number " + (i + 1) + ": ");
+                userNumbers[i] = scnr01.nextInt();
+            }
+
+            int product = calculateUserNumbers(userNumbers, 4);
+
+            System.out.println("The product of your numbers is: " + product);
+
+            scnr01.close();
+
+        }
+    }
+}
