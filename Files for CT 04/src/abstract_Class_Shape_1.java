aadbb11745c0da121d983c04e6cc7ac87dd1d56d
skipvicente <skipvicente2@aol.com>
2024-06-03 23:43:16 -0500

    Updates for CT 04 & 05 at 11:54 with user input for shapes

--- Files for CT 04/src/abstract_Class_Shape_1.java
+++ Files for CT 04/src/abstract_Class_Shape_1.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+
 public class abstract_Class_Shape_1 {
     public static void main(String[] args) {
+        Scanner scnr01 = new Scanner(System.in);
         Shape[] myShapeArray = new Shape[3];
+        double userNum01;
+        double userNum02;
 
-        myShapeArray[0] = new Sphere(4.5);
-        myShapeArray[1] = new Cylinder(3.5, 6.5);
-        myShapeArray[2] = new Cone(5.0, 7.5);
+        /* Sphere */
+        System.out.println("Enter Sphere radius:");
+        userNum01 = scnr01.nextDouble();
+        myShapeArray[0] = new Sphere(userNum01);
+
+        /* Cylinder */
+        System.out.println("Enter Cylinder radius:");
+        userNum01 = scnr01.nextDouble();
+        System.out.println("Enter Cylinder height:");
+        userNum02 = scnr01.nextDouble();
+        myShapeArray[1] = new Cylinder(userNum01, userNum02);
+
+        /* Cone */
+        System.out.println("Enter Cone radius:");
+        userNum01 = scnr01.nextDouble();
+        System.out.println("Enter Cone height:");
+        userNum02 = scnr01.nextDouble();
+        myShapeArray[2] = new Cone(userNum01, userNum02);
 
         for (int i = 0; i < 3; i++)
             System.out.println(myShapeArray[i]);
+
+        scnr01.close();
     }
 }
--- Files for CT 05/src/Main.java
+++ Files for CT 05/src/Main.java
@@ -1,11 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+/* //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scnr01 = new Scanner(System.in);
 
         int[] userNumbers = new int[5]; /* Only using 5 because we only want to permit 5 elements in array */
-        int count = 1;
+ /*       int count = 1;
 
         System.out.println("Enter number " + count + " of 5 " + "\n");
 
@@ -23,3 +23,4 @@ public class Main {
         }
     }
 }
+*/
