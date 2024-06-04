2062b652466f21795de1839438b5431f734b8bab
skipvicente <skipvicente2@aol.com>
2024-06-03 21:51:42 -0500

    Updates for CT 04 9:51 unable to run on inteliJ .. "error" java: warning: source release 17 requires target release 17"

--- Files for CT 04/src/abstract_Class_Shape_1.java
+++ Files for CT 04/src/abstract_Class_Shape_1.java
@@ -7,6 +7,6 @@ public class abstract_Class_Shape_1 {
         myShapeArray[2] = new Cone(5.0, 7.5);
 
         for (int i = 0; i < 3; i++)
-            System.out.println(myShapeArray[i].toString());
+            System.out.println(myShapeArray[i]);
     }
 }
