9d7b0c77dacf3c869bbfd9d6b55a3ca936de7845
skipvicente <skipvicente2@aol.com>
2024-06-03 20:57:51 -0500

    Updates for CT 04 8:57

--- Files for CT 04/src/Shape.java
+++ Files for CT 04/src/Shape.java
@@ -7,8 +7,24 @@ class Sphere extends Shape {
 
     private double radius;
     public Sphere (double radius){
-        this.radius - radius;
+        this.radius = radius;
     }
 
+    public String toString()
+    {
+        System.out.println("Test surface area: " + surface_area());
+        System.out.println("Test volume: " + volume());
+        return "The spheres surface area is: " + surface_area() + " The spheres volume is: " + volume();
+    }
+
+    public double surface_area() /* Here we calculate the surface area */
+    {
+        return 4.0 * Math.PI * radius * radius;
+    }
+
+    public double volume() /* Here we calculate the volume */
+    {
+        return 4.0 * Math.PI * radius * radius * radius / 3.0;
+    }
 }
 
