e7334065ab5b735769e4e7f319c8a446f79e9a09
skipvicente <skipvicente2@aol.com>
2024-06-08 21:56:40 -0500

    Update 9:56 SuperBank only

--- src/SuperBank.java
+++ src/SuperBank.java
@@ -1,4 +1,36 @@
 package BankMod01
 
 public class SuperBank {
+
+    String firstName;
+    String lastName;
+    Integer accountID;
+    Double balance;
+
+    public SuperBank(){
+        this.balance = 0.0;
+    }
+
+    /* Getter-Setter First name */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /* Getter-Setter Last name */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAccountID(Integer accountID) {
+        this.accountID = accountID;
+    }
+
 }
